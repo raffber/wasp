2d879eaf7ef5a7d635459711ae3d095b59c1f180
Raphael Bernhard <beraphae@gmail.com>
2015-04-30 22:18:17 +0200

    metadata: documentation

--- src/wasp/metadata.py
+++ src/wasp/metadata.py
@@ -3,6 +3,22 @@ from . import decorators
 
 
 class Metadata(Serializable):
+    """
+    Storage class for project metadata. It allows
+    setting arbitrary string key-value pairs which can
+    then be accessed using ``ctx.meta``. This information
+    may be used by external tools for example to create an
+    installable package.
+
+    Metadata can be set either within a build script or using a
+    wasprc.json file.
+
+    Note: This class always contains the following keys (with at least
+    a sensible default value)::
+
+        * projectname (defaults to 'myproject')
+        * projectid (to be used as name in the filesystem. defaults to 'myproject')
+    """
     projectname = 'myproject'
     projectid = None
     _other = {}
@@ -51,6 +67,11 @@ class Metadata(Serializable):
 
 
 class metadata(FunctionDecorator):
+    """
+    Decorator for annotating a function, which is called by
+    ``wasp``. This function should return a :class:`Metadata`
+    object to be stored in ``ctx.meta``.
+    """
     def __init__(self, f):
         super().__init__(f)
         decorators.metadata = f
