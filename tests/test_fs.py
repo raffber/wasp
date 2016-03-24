2dd96e7d5ea44f7e0ed9ffb88a16cffaef3c7fec
Raphael Bernhard <beraphae@gmail.com>
2016-03-24 20:23:03 +0100

    update unittests

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,7 +1,7 @@
 from wasp import directory, Directory, file, factory
 
 curdir = directory(__file__)
-testdir = curdir.join('test-dir')
+testdir = directory(curdir.join('test-dir'))
 
 
 def prepare():
@@ -17,22 +17,26 @@ def test_directory():
     assert subdir.exists
     assert isinstance(testdir.join('subdir'), Directory)
     home_dir = directory('~')
-    assert subdir.relative(home_dir).absolute == subdir.absolute
+    assert subdir.relative(home_dir).absolute.path == subdir.absolute.path
     for f in ['a.txt', 'dira/dirb/b.txt', 'c.txt']:
-        f = testdir.join(file(f))
+        f = file(testdir.join(f))
         directory(f).ensure_exists()
         assert directory(f).exists
         with open(f.path, 'w') as fwrite:
             fwrite.write('asdf')
-        assert file(f).exists
-    fs = [x for x in testdir.glob('.*', dirs=False)]
-    assert fs == ['a.txt', 'dira/dirb/b.txt', 'c.txt']
+        assert f.exists
+    fs = [x.relative(testdir).path for x in testdir.glob('.*', dirs=False)]
+    assert set(fs) == {'a.txt', 'dira/dirb/b.txt', 'c.txt'}
+    fs = [x.relative(testdir).path for x in testdir.glob('.*', recursive=False)]
+    assert set(fs) == {'a.txt', 'dira', 'c.txt', 'subdir'}
+    fs = [x.relative(testdir).path for x in testdir.glob('.*', recursive=False, exclude='subdir')]
+    assert set(fs) == {'a.txt', 'dira', 'c.txt'}
 
 
 def test_serialize():
     home_dir = directory('~')
     new_home_dir = factory.from_json(home_dir.to_json())
-    assert home_dir.path == new_home_dir
+    assert home_dir.path == new_home_dir.path
     assert home_dir.relative_to == new_home_dir.relative_to
 
 
