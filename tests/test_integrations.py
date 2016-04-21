89ac4ab474781fe86b0adf4569af5f1b939150ed
Raphael Bernhard <beraphae@gmail.com>
2016-04-21 08:47:46 +0200

    improve integration tests

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -1,4 +1,4 @@
-from wasp import osinfo, directory, ctx
+from wasp import osinfo, directory
 from wasp.shell import run
 
 
@@ -25,25 +25,51 @@ def test_build():
     # check if jsmin was installed by npm
     assert any('jsmin@' in l for l in out_lines)
     bd = directory(topdir.join('build'))
-    assert bd.join('main').exists
+    main_file = bd.join('main').absolute
+    assert main_file.exists
     assert bd.join('buildtest').exists
     assert bd.join('node_modules').exists
-    main_file = bd.join('main').path
-    exit_code, proc_out = run(main_file)
+    exit_code, proc_out = run(main_file.path)
     out = proc_out.stdout.split('\n')
     assert 'Hello, World!!' in out
     assert bd.join('node_modules/jsmin/bin/jsmin').exists
+    exit_code, proc_out = run('./wasp build', cwd=topdir.path)
+    outs = proc_out.stdout.split('\n')
+    assert '[SUCC]  Command: `configure`' in outs
+    assert '[SUCC]  Command: `build`' in outs
+    assert len(outs) == 2
 
 
-def test_nodebuild():
+def test_rustbuild():
+    if not osinfo.linux or not check_exists():
+        return
+    topdir = directory(__file__).join('..').absolute
+    directory(topdir.join('build')).remove()
+    exit_code, proc_out = run('./wasp rust', cwd=topdir.path)
+    out_lines = proc_out.stdout.split('\n')
+    err_lines = proc_out.stderr.split('\n')
+    assert len(err_lines) == 1 and err_lines[0] == ''
+    assert '[SUCC]  Command: `rust`' in out_lines
+    assert any('rustc' in l for l in out_lines)
+    exit_code, proc_out = run('./wasp rust', cwd=topdir.path)
+    out_lines = proc_out.stdout.split('\n')
+    assert len(out_lines) == 2
+    exit_code, proc_out = run('./wasp clean rust', cwd=topdir.path)
+    out_lines = proc_out.stdout.split('\n')
+    assert any('rustc' in l for l in out_lines)
+    assert '[SUCC]  Command: `rust`' in out_lines
+
+
+def test_cppbuild():
     if not osinfo.linux or not check_exists():
         return
 
 
-def test_rustbuild():
+def test_qtbuild():
     if not osinfo.linux or not check_exists():
         return
 
 
 if __name__ == '__main__':
     test_build()
+    test_rustbuild()
